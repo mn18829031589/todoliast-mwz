523f8ecae65eac8ad8fad188ba664a64939a4541
mn18829031589 <1791397782@qq.com>
2020-04-12 19:12:20 +0800

    2

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import TodoList from './TodoList';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <h1> Todo List </h1>
+    <TodoList />
   </React.StrictMode>,
   document.getElementById('root')
 );
